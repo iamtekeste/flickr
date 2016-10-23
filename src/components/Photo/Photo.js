251c59336acc9b0452ce9db56fe907a53d0eaf42
Tekeste G <tekeste.kidanu@gmail.com>
2016-10-23 09:48:45 -0400

    add photo captions

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -3,11 +3,17 @@ import Utils from '../../Utils';
 
 class Photo extends Component {
     buildPhotoURL(photo) {
-        //this.getPhotoSizes(photo.id);
-        return `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_z.jpg`
+        // this.getPhotoSizes(photo.id);
+        let baseURL = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}`;
+        return  {
+            small: `${baseURL}_q.jpg`,
+            medium500: `${baseURL}.jpg`,
+            medium640: `${baseURL}_z.jpg`,
+            large: `${baseURL}_b.jpg`
+        }
     }
     getPhotoSizes(id) {
-        let apiURL = `${Utils.baseURL}&api_key=${Utils.apiKey}&method=flickr.photos.getSizes&photo_id=${id}`;
+        let apiURL = `${Utils.baseURL}&method=flickr.photos.getSizes&photo_id=${id}`;
         fetch(apiURL)
             .then(response => {
                 return response.json();
@@ -19,10 +25,11 @@ class Photo extends Component {
 
     render() {
         let photo = this.props.photo;
-        let photoURL = this.buildPhotoURL(photo)
+        let photoURL = this.buildPhotoURL(photo);
         return (
             <div className="thumbnail">
-                <img src={photoURL} alt={photo.title} />
+                    <img src={photoURL.medium500} alt={photo.title} data-big={photoURL.large}/>
+                    <div className="caption">{photo.title}</div>
             </div>
         )
     }
