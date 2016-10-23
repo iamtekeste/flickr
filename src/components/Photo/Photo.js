5f7a5c635387f5a438393463252fb1df8672300c
Tekeste G <tekeste.kidanu@gmail.com>
2016-10-23 17:39:30 -0400

    handle large version of photo

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -2,15 +2,37 @@ import React, { Component } from 'react';
 import Utils from '../../Utils';
 
 class Photo extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            sizes: {}
+        }
+        this.showLargePhoto = this.showLargePhoto.bind(this);
+    }
+    componentDidMount() {
+        let sizes = this.buildPhotoURL(this.props.photo);
+        this.setState({
+            sizes: sizes
+        })
+    }
     buildPhotoURL(photo) {
         // this.getPhotoSizes(photo.id);
         let baseURL = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}`;
-        return  {
+       
+        return {
             small: `${baseURL}_q.jpg`,
             medium500: `${baseURL}.jpg`,
             medium640: `${baseURL}_z.jpg`,
             large: `${baseURL}_b.jpg`
         }
+    }
+    showLargePhoto() {
+        let selectedPhoto = {
+            url: this.state.sizes.large,
+            alt: this.props.photo.title
+        }
+        this.props.showSelectedPhoto(selectedPhoto);
     }
     getPhotoSizes(id) {
         let apiURL = `${Utils.baseURL}&method=flickr.photos.getSizes&photo_id=${id}`;
@@ -25,10 +47,10 @@ class Photo extends Component {
 
     render() {
         let photo = this.props.photo;
-        let photoURL = this.buildPhotoURL(photo);
+        // let photoURL = this.buildPhotoURL(photo);
         return (
             <div className="thumbnail">
-                    <img src={photoURL.medium500} alt={photo.title} data-big={photoURL.large}/>
+                    <img src={this.state.sizes.medium500} alt={photo.title} onClick={this.showLargePhoto}/>
                     <div className="caption">{photo.title}</div>
             </div>
         )
